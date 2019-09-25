fbbff456ff0ab2022c62ba889fc6aa97235d0a44
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2019-09-25 17:14:44 +0700

    update cors

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const logger = require('morgan');
-const router = require('./routes/root');
-const helmet = require('helmet');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const logger = require("morgan");
+const router = require("./routes/root");
+const helmet = require("helmet");
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
-app.use(helmet({xssFilter:true}));
-app.use('/api', router);
+app.use(cors("*"));
+app.use(helmet({ xssFilter: true }));
+app.use("/api", router);
 
 app.listen(port, () => {
-    console.log(`App is listening on port ${port}`)
+  console.log(`App is listening on port ${port}`);
 });
 
 module.exports = app;
--- configs/db.js
+++ configs/db.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mysql = require('mysql');
+const mysql = require("mysql");
 const conn = mysql.createConnection({
-    host : process.env.DB_HOST ,
-    user: process.env.DB_USER ,
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_DATABASE 
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
 
-conn.connect((err) => {
-    if(err) console.log(err);
-})
+conn.connect(err => {
+  if (err) console.log(err);
+  console.log("db connected...");
+});
 
 module.exports = conn;
